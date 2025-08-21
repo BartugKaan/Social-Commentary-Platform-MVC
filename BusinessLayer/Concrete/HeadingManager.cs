a83c3dc42ab2189cf85cf79df27978ab8240abe6
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-08-21 17:00:49 +0300

    Add Heading management functionality
    Introduced `IHeadingService` and `HeadingManager` for managing `Heading` entities. Defined `IContactDal` for data access, implemented in `EFHeadingDal`. Updated `AdminHeadingController` to use `HeadingManager` for adding headings. Enhanced views `AddHeading.cshtml`, `ContentByHeading.cshtml`, and `Index.cshtml` to support new functionality while adhering to SOLID principles and the existing architecture.

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager : IHeadingService
+    {
+        IContactDal _headingDal;
+
+        public HeadingManager(IContactDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+
+        public void AddHeading(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public void DeleteHeading(Heading heading)
+        {
+            _headingDal.Delete(heading);
+        }
+
+        public List<Heading> GetAll()
+        {
+            return _headingDal.List();
+        }
+
+        public Heading GetById(int id)
+        {
+            return _headingDal.Get(x => x.HeadingId == id);
+        }
+
+        public void UpdateHeading(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/AdminHeadingController.cs
@@ -0,0 +1,73 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminHeadingController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EFHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+        WriterManager writerManager = new WriterManager(new EFWriterDal());
+
+        public ActionResult Index()
+        {
+            var headingValues = headingManager.GetAll();
+            return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in categoryManager.GetAll()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.ctg = valueCategory;
+            List<SelectListItem> valueWriter = (from x in writerManager.GetAll()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName,
+                                                    Value = x.WriterId.ToString()
+                                                }).ToList();
+            ViewBag.wrt = valueWriter;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            HeadingValidator validationRules = new HeadingValidator();
+            ValidationResult results = validationRules.Validate(heading);
+            if (results.IsValid)
+            {
+                heading.HeadingDate =DateTime.Parse(DateTime.Now.ToShortDateString());
+                headingManager.AddHeading(heading);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+
+        public ActionResult ContentByHeading()
+        {
+            return View();
+        }
+    }
+}
