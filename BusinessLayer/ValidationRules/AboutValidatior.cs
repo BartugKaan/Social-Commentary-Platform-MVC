47b9b7ecc34aba4b29b1940bdb4fc08cb05ca188
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-08-22 16:26:25 +0300

    Add About management feature with validation
    This commit introduces the `IAboutService` interface and its implementation `AboutManager` for managing "About" entities. It also creates the `IAboutDal` interface and the `EFAboutDal` class for data access.
    
    The `AdminAboutController` is added to handle CRUD operations for "About" entries. New views (`Index.cshtml`, `EditAbout.cshtml`, `AboutPartial.cshtml`) are created, and the layout is updated to include navigation to the "About" section.
    
    Validation for "About" entries is implemented using FluentValidation in the `AboutValidatior` class to ensure required fields are filled.
    
    Project files are updated to include the new classes and views, maintaining compliance with the SOLID principles and the existing architecture.

---
+++ BusinessLayer/ValidationRules/AboutValidatior.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidatior : AbstractValidator<About>
+    {
+        public AboutValidatior()
+        {
+            RuleFor(x => x.AboutDetails1)
+                .NotEmpty().WithMessage("Lütfen Hakkında kısmını boş geçmeyiniz");
+            RuleFor(x => x.AboutDetails2)
+                .NotEmpty().WithMessage("Lütfen Hakkında kısmını boş geçmeyiniz");
+            //RuleFor(x => x.AboutImage1).NotEmpty().WithMessage("Lütfen Hakkında kısmını boş geçmeyiniz");
+            //RuleFor(x => x.AboutImage2).NotEmpty().WithMessage("Lütfen Hakkında kısmını boş geçmeyiniz");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/AdminAboutController.cs
@@ -0,0 +1,93 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        AboutManager manager = new AboutManager(new EFAboutDal());
+        public ActionResult Index()
+        {
+            var aboutValues = manager.GetAll();
+            return View(aboutValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            AboutValidatior validations = new AboutValidatior();
+            ValidationResult result = validations.Validate(about);
+            if (result.IsValid)
+            {
+                manager.AddAbout(about);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // TODO: It is not working properly Needs to be fixed
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                ViewBag.error = "Hata: Yeni Hakkımıza oluştururken Hata oluştu. Tüm gerekli alanları doldurunuz.";
+            }
+            return View();
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
+
+
+        [HttpGet]
+        public ActionResult EditAbout(int id)
+        {
+            var aboutValue = manager.GetById(id);
+            return View(aboutValue);
+        }
+
+
+        [HttpPost]
+        public ActionResult EditAbout(About about)
+        {
+            AboutValidatior validations = new AboutValidatior();
+            ValidationResult result = validations.Validate(about);
+            if (result.IsValid)
+            {
+                manager.UpdateAbout(about);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+
+        public ActionResult DeleteAbout(int id)
+        {
+            var aboutValue = manager.GetById(id);
+            manager.DeleteAbout(aboutValue);
+            return RedirectToAction("Index");
+        }
+    }
+}
